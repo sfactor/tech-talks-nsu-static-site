523f0bfcbbeaa6946d1edfaf22aa03462b5d5ae9
Sam Faktorovich <sam@Sams-MacBook-Pro.local>
2016-03-14 19:03:27 +0600

    work in progress

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,3 +1,4 @@
 export website_root=
 export ftp_website_root=/
+cp data/lectures.json source
 middleman build && middleman deploy
--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,3 +1,4 @@
 export website_root=/staging
 export ftp_website_root=/staging
+cp data/lectures.json source
 middleman build && middleman deploy
